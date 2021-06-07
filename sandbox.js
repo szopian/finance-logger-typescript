89d0dbab2420e3ac919d84fd923a0a565f6d6a8a
Adri Ana <szopiangreg@gmail.com>
2021-06-07 13:49:29 +0100

    strict values

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,12 @@
 console.log("tsc sandbox.ts");
 console.log("tsc sandbox.ts -w");
-var circ = function (diameter) {
-    return diameter * Math.PI;
-};
-console.log(circ(1));
+// const circ = (diameter: number) => {
+//   return diameter * Math.PI;
+// };
+// console.log(circ(1));
+var letters = ["a", "b", "c"];
+// letters.push(1);
+letters.push("d");
+console.log(letters);
+var numbers = [1, 2, 3];
+var numters = [1, "a", 2, "b"];
--- sandbox.ts
+++ sandbox.ts
@@ -1,8 +1,16 @@
 console.log("tsc sandbox.ts");
 console.log("tsc sandbox.ts -w");
 
-const circ = (diameter: number) => {
-  return diameter * Math.PI;
-};
+// const circ = (diameter: number) => {
+//   return diameter * Math.PI;
+// };
 
-console.log(circ(1));
+// console.log(circ(1));
+
+let letters = ["a", "b", "c"];
+// letters.push(1);
+letters.push("d");
+console.log(letters);
+
+let numbers = [1, 2, 3];
+let numters = [1, "a", 2, "b"];
