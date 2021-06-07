52e1187e220f2051f89e229e19ee543af8fa1b85
Adri Ana <szopiangreg@gmail.com>
2021-06-07 13:40:20 +0100

    const circ = (diameter: number) => {   return diameter * Math.PI; };

--- sandbox.js
+++ sandbox.js
@@ -1,2 +1,6 @@
 console.log("tsc sandbox.ts");
 console.log("tsc sandbox.ts -w");
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(1));
--- sandbox.ts
+++ sandbox.ts
@@ -1,2 +1,8 @@
 console.log("tsc sandbox.ts");
 console.log("tsc sandbox.ts -w");
+
+const circ = (diameter: number) => {
+  return diameter * Math.PI;
+};
+
+console.log(circ(1));
