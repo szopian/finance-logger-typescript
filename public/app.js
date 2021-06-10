c9e150ee90124c3f3cdaa5020029705173340443
Adri Ana <szopiangreg@gmail.com>
2021-06-10 22:29:43 +0100

    add event listier

--- public/app.js
+++ public/app.js
@@ -3,9 +3,13 @@
 // console.log(anchor.href);
 // const form =document.querySelector('form')!;
 var form = document.querySelector(".new-item-form");
-console.log(form.children);
+// console.log(form.children);
 //inputs
 var type = document.querySelector("#type");
 var toform = document.querySelector("#toform");
 var details = document.querySelector("#details");
 var amount = document.querySelector("#amount");
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    console.log(type.value, toform.value, details.value, amount.value);
+});
--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,16 @@
 // const form =document.querySelector('form')!;
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
 
-console.log(form.children);
+// console.log(form.children);
 
 //inputs
 const type = document.querySelector("#type") as HTMLSelectElement;
 const toform = document.querySelector("#toform") as HTMLInputElement;
 const details = document.querySelector("#details") as HTMLInputElement;
 const amount = document.querySelector("#amount") as HTMLInputElement;
+
+form.addEventListener("submit", (e: Event) => {
+  e.preventDefault();
+
+  console.log(type.value, toform.value, details.value, amount.value);
+});
