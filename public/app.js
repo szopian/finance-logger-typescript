04dcb99b1468b76665e915309c920c42c1c4342d
Adri Ana <szopiangreg@gmail.com>
2021-06-10 22:21:00 +0100

    adding inputs selectors

--- public/app.js
+++ public/app.js
@@ -1,2 +1,11 @@
 "use strict";
-console.log("TEST");
+// const anchor = document.querySelector("a")!;
+// console.log(anchor.href);
+// const form =document.querySelector('form')!;
+var form = document.querySelector(".new-item-form");
+console.log(form.children);
+//inputs
+var type = document.querySelector("#type");
+var toform = document.querySelector("#toform");
+var details = document.querySelector("#details");
+var amount = document.querySelector("#amount");
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
-console.log("TEST");
+// const anchor = document.querySelector("a")!;
+// console.log(anchor.href);
+
+// const form =document.querySelector('form')!;
+const form = document.querySelector(".new-item-form") as HTMLFormElement;
+
+console.log(form.children);
+
+//inputs
+const type = document.querySelector("#type") as HTMLSelectElement;
+const toform = document.querySelector("#toform") as HTMLInputElement;
+const details = document.querySelector("#details") as HTMLInputElement;
+const amount = document.querySelector("#amount") as HTMLInputElement;
