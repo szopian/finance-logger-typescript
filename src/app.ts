818b49955b63c9ada2479d191294500255256659
Adri Ana <szopiangreg@gmail.com>
2021-06-11 14:39:12 +0100

    job done;)

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,7 @@
 import { Invoice } from "./classes/invoice.js";
+import { ListTemplate } from "./classes/ListTemplate.js";
 import { Payment } from "./classes/Payment.js";
 import { HasFormatter } from "./interfaces/HasFormatter.js";
-
-// let docOne: HasFormatter;
-// let docTwo: HasFormatter;
-
-// docOne = new Invoice("greg", "web work", 350);
-// docTwo = new Payment("adri", "platform work", 550);
-
-// let docs: HasFormatter[] = [];
-// docs.push(docOne);
-// docs.push(docTwo);
-
-// console.log(docs);
-
-// const invOne = new Invoice("greg", "work on website", 250);
-// const invTwo = new Invoice("adri", "work on platform", 450);
-
-// let invoices: Invoice[] = [];
-// invoices.push(invOne);
-// invoices.push(invTwo);
-
-// invoices.forEach((inv) => {
-//   console.log(inv.client, inv.amount, inv.format());
-// });
 
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
 
@@ -32,6 +10,10 @@ const type = document.querySelector("#type") as HTMLInputElement;
 const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
 const details = document.querySelector("#details") as HTMLInputElement;
 const amount = document.querySelector("#amount") as HTMLInputElement;
+
+//List template instance
+const ul = document.querySelector("ul")!;
+const list = new ListTemplate(ul);
 
 form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
@@ -43,5 +25,5 @@ form.addEventListener("submit", (e: Event) => {
     doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
   }
 
-  console.log(doc);
+  list.render(doc, type.value, "start");
 });
